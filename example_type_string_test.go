c78ef29b9823ff115dd08da4512d534a07051b78
Charles Iliya Krempeaux <iamreiver@gmail.com>
2018-08-05 21:50:48 -0700

    create example

--- example_type_string_test.go
+++ example_type_string_test.go
@@ -10,8 +10,9 @@ func ExampleType_String() {
 
 	var colorspace fourcc.Type = fourcc.FOURCC('P','A','L','8')
 
+	s := colorspace.String()
 
-	fmt.Println(colorspace.String())
+	fmt.Println(s)
 
 	// Output:
 	// PAL8
