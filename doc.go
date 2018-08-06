0e94f547b600b9bf3a17f0d3f207c48b22e13074
Charles Iliya Krempeaux <iamreiver@gmail.com>
2018-08-05 21:22:11 -0700

    added more docs

---
+++ doc.go
@@ -0,0 +1,10 @@
+/*
+Package fourcc provides tools for working with FOURCC (four character code) (4CC) identifiers.
+
+FOURCC (also sometimes called 4CC) is short for "four character code".
+
+FOURCC is an identifier for a video codec, compression format, color or pixel format used in media files.
+
+A character in this context is a 1 byte/8 bit value, thus a FOURCC always takes up exatly 32 bits/4 bytes in a file.
+*/
+package fourcc
