30541c926387389e4a10075506076a8056017ead
Charles Iliya Krempeaux <iamreiver@gmail.com>
2018-08-05 21:45:09 -0700

    fourcc.Uint32()

---
+++ uint32.go
@@ -0,0 +1,9 @@
+package fourcc
+
+// Uint32 turns 4 characters (where characters are uint8), into a (compact) machine readble version of a FOURCC (as an uint32).
+func Uint32(a uint8, b uint8, c uint8, d uint8) uint32 {
+
+	var value uint32 = (uint32(a) | (uint32(b) << 8) | (uint32(c) << 16) | (uint32(d) << 24))
+
+	return value
+}
