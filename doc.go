74ebb4f0d066796fb141508e8cf1b05ed39ef3b6
Charles Iliya Krempeaux <iamreiver@gmail.com>
2018-08-05 21:42:15 -0700

    added more docsush

--- doc.go
+++ doc.go
@@ -6,5 +6,11 @@ FOURCC (also sometimes called 4CC) is short for "four character code".
 FOURCC is an identifier for a video codec, compression format, color or pixel format used in media files.
 
 A character in this context is a 1 byte/8 bit value, thus a FOURCC always takes up exatly 32 bits/4 bytes in a file.
+
+
+See Also
+
+More information about FOURCC can be found at: https://www.fourcc.org/
+
 */
 package fourcc
