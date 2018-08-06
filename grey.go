69095e2101a447c5b3b10beb0fdebd22569466c9
Charles Iliya Krempeaux <iamreiver@gmail.com>
2018-08-06 00:58:45 -0700

    added some FOURCCs

---
+++ grey.go
@@ -0,0 +1,11 @@
+package fourcc
+
+// Grey formats
+const (
+	GREY = Const("GREY") // depth:  8; description: Greyscale
+	Y4   = Const("Y04 ") // depth:  4; description: Greyscale
+	Y6   = Const("Y06 ") // depth:  6; description: Greyscale
+	Y10  = Const("Y10 ") // depth: 10; description: Greyscale
+	Y12  = Const("Y12 ") // depth: 12; description: Greyscale
+	Y16  = Const("Y16 ") // depth: 16; description: Greyscale
+)
